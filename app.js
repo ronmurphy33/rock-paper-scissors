91ed12b6ce4701d89981bbc1e4e51feacaf07d2c
Ron Murphy <ronmurphy33@gmail.com>
2022-11-25 15:17:25 -0500

    changed playround function, computer choice function and player choice promp

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 function getComputerChoice(){
-    const gameChoices = ['rock', 'paper', 'scissors'];
-    return gameChoices[Math.floor(Math.random()*3)];
+    let choicesArr = ['rock', 'paper', 'scissors'];
+    return choicesArr[Math.floor(Math.random()*3)]
 }
 
-console.log(getComputerChoice());
-let playerInput = window.prompt('rock, paper, or scissors?').toLowerCase();
-console.log(playerInput);
+function playRound(){
+    let playerChoice = prompt('rock, paper, or scissors?').toLowerCase();
+    let computerChoice = getComputerChoice();
+    console.log('player: '+ playerChoice);
+    console.log('computer: '+ computerChoice); 
+    if (playerChoice == computerChoice){
+        console.log('Tie! Try again')
+    } else 
+    if (((playerChoice ==='rock' && computerChoice === 'scissors'))
+    || (playerChoice === 'paper' && computerChoice === 'rock')
+    || (playerChoice === 'scissors' && computerChoice === 'paper')) {
+        console.log('You win! '+playerChoice+ ' beats '+ computerChoice)
+    } else 
+    console.log("You lose! "+computerChoice+ " beats "+playerChoice);
+};
+
+playRound();
