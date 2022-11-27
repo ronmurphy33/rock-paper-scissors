12bd72a8ae4dc0f89bf4597e14ef82db752a35b4
Ron Murphy <ronmurphy33@gmail.com>
2022-11-27 14:56:37 -0500

    added playerChoice, playGame, and gameEnd functions. addedscoring and rounds

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let playerScore = 0;
 let computerScore = 0;
-let round = 0;
-let playerChoice = prompt('rock, paper, or scissors?').toLowerCase();
+let round = 1;
+let playerChoice = getPlayerChoice();
 let computerChoice = getComputerChoice();
 
 function getComputerChoice(){
@@ -9,26 +9,53 @@ function getComputerChoice(){
     return choicesArr[Math.floor(Math.random()*3)]
 }
 
+function getPlayerChoice(){
+    let choice = prompt('rock, paper, or scissors?');
+    return choice.toLowerCase();
+}
+
+function gameEnd(){
+    if (playerScore > computerScore){
+        console.log('You win '+playerScore+' to '+computerScore+'. Thanks for playing!')
+    } else 
+    if (playerScore === computerScore){
+        console.log('Its a tie! Thanks for playing')
+    } else 
+    {
+        console.log ('You lose '+playerScore+' to '+computerScore+'submit to your machine overlord')
+    }
+}
+
 function playRound(playerChoice, computerChoice){
     console.log('player: '+ playerChoice);
     console.log('computer: '+ computerChoice); 
     if (playerChoice == computerChoice){
-        return ('Tie! Try again');
+        console.log(('Tie! Try again'));
+        return 'tie';
     } else 
     if (((playerChoice ==='rock' && computerChoice === 'scissors'))
     || (playerChoice === 'paper' && computerChoice === 'rock')
     || (playerChoice === 'scissors' && computerChoice === 'paper')) {
-        playerScore++;
-        return ('You win! '+playerChoice+ ' beats '+ computerChoice)
+        console.log(('You win! '+playerChoice+ ' beats '+ computerChoice));
+        return 'player';
     } else ;
-    computerScore ++;
-    return ("You lose! "+computerChoice+ " beats "+playerChoice)
+    console.log(("You lose! "+computerChoice+ " beats "+playerChoice));
+    return 'computer';
 };
 
 function playGame(){
-    for (var i = 0; i < 6; i++){
-        playRound();
+    for (var i = 0; i < 5; i++){
+        let results = playRound(getPlayerChoice(), getComputerChoice());
+        if (results == "computer"){
+            computerScore ++
+        } else 
+        if (results == "player"){
+            playerScore ++
+        }
+    console.log ('Player: '+playerScore+' Computer: '+computerScore+ ' Rounds left: '+(5-round));
+    round++;
     }
+    gameEnd();
 }
 
 playGame();
